4bfa7b1ca00cf8531c5415f78063771045ccc266
Harshal Pandya <harshalgem@gmail.com>
2013-02-24 22:39:36 -0500

    Added abstract class Tree

--- src/main/scala/com/github/harshal/dsexp/tree/Tree.scala
+++ src/main/scala/com/github/harshal/dsexp/tree/Tree.scala
@@ -10,5 +10,9 @@ case class Node[T](value:T,right:Node,left:Node)
 abstract class Tree(root:Node){
   def add(node:Node)
   def remove(node:Node)
-
+  def depth
+  def size
+  def preorder:Seq[Node]
+  def inorder:Seq[Node]
+  def postorder:Seq[Node]
 }
